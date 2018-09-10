aefba6989247d351b59736b6971e9fdb6d3d05f7
nascal3 <nascal3@gmail.com>
2018-09-10 21:29:40 +0300

    added more time on the loader disapear timeout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -84,7 +84,7 @@ export class HomePage {
 
         setTimeout(() => {
           loader.dismiss();
-        }, 2000)
+        }, 3000)
       });
     })
   }
@@ -140,7 +140,7 @@ export class HomePage {
 
       setTimeout(() => {
         loader.dismiss();
-      }, 2000)
+      }, 3000)
 
     });
 
